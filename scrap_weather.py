4de1996e37b63dd347a5f933e4ad4ad3ecf3115a
Maciek <macblawat@gmail.com>
2025-08-21 16:36:41 +0200

    Scrapper of the  weather data v1

--- scrap_weather.py
+++ scrap_weather.py
@@ -4,5 +4,26 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # loads from .env
+load_dotenv()  
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
+if not API_KEY:
+    raise ValueError("API key not found.")
+
+cities = ["Warsaw", "Krakow", "Gdansk", "Wroclaw", "Poznan",
+          "Lodz", "Szczecin", "Lublin", "Katowice", "Bialystok"]
+
+for city in cities:
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},PL&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        
+        filename = f"weather_data/weather_{city}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+
+        with open(filename, "w", encoding="utf-8") as f:
+         json.dump(data, f, indent=4, ensure_ascii=False)
+
+        print(f"Saved weather data for {city} -> {filename}")
+    else:
+        print(f"Error fetching data for {city}: {response.status_code} - {response.text}")
