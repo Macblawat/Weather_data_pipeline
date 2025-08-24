d7865efa7011afd03677b5aa0d2ccad3e98b1be0
Macblawat <macblawat@gmail.com>
2025-08-25 01:02:30 +0200

    Scrapper v3

--- scrap_weather.py
+++ scrap_weather.py
@@ -2,27 +2,41 @@ import requests
 import json
 from datetime import datetime
 import os
+import logging
 from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 load_dotenv()  
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+
+
 if not API_KEY:
     raise ValueError("API key not found.")
 
 cities = ["Warsaw", "Krakow", "Gdansk", "Wroclaw", "Poznan",
           "Lodz", "Szczecin", "Lublin", "Katowice", "Bialystok"]
 
+os.makedirs("weather_data", exist_ok=True)
+
+weather_data = {}
+
 for city in cities:
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city},PL&appid={API_KEY}&units=metric"
-    response = requests.get(url)
     
-    if response.status_code == 200:
+    try:
+        response = requests.get(url,timeout=10)
+        response.raise_for_status()
+        weather_data[city] = response.json()
+        logging.info(f"Scrapped weather for {city}")
         
-        filename = f"weather_data/weather_{city}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
-
-        with open(filename, "w", encoding="utf-8") as f:
-         json.dump(response.json(), f, ensure_ascii=False)
-
-        print(f"Saved weather data for {city} -> {filename}")
-    else:
-        print(f"Error getting data for {city}: {response.status_code} - {response.text}")
+    except requests.exceptions.RequestException as e:
+           logging.error(f"Error getting data for {city}: {e}")
+           
+if weather_data:
+    filename = f"weather_data/weather_batch_{datetime.now().strftime('%Y%m%d_%H%M')}.json"
+    
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(weather_data, f, ensure_ascii=False, indent=2)
+    logging.info(f"Saved batch weather data -> {filename}")
